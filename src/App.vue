d2e5c4c7d85bc82f3380e720c3c186644cc7ffcb
Bruno Dias <brunodg.dev@gmail.com>
2025-03-21 15:19:01 -0300

    Initial commit. [feat] initial setup for Vue 3 + TresJS + Three.js

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TresScene from './components/TresScene.vue'
+</script>
+
+<template>
+  <TresScene />
+</template>
---
+++ src/components/TresScene.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+    <TresCanvas clear-color="#1e1e1e">
+        <!-- Adicionaremos objetos aqui nas aulas-->
+    </TresCanvas>
+</template>
